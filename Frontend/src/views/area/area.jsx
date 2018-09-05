b45f7d98632faaddb62198b5ebaed149bbe2a412
Facundo Juarez <facundoalejojuarez@gmail.com>
2018-09-05 18:56:15 -0300

    se acomoda estetica para que se paresca al resto de las paginas

--- Frontend/src/views/area/area.jsx
+++ Frontend/src/views/area/area.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 // Componentes
 import TablaPosiciones from './secciones/tablaPosiciones.jsx';
+import GridContainer from '../../components/grid/gridContainer.jsx';
+import GridItem from '../../components/grid/gridItem.jsx';
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// Estilos
+import estilos from '../../assets/styles/views/area/areaStyle.jsx';
+
 // Views
 import Layout from '../layout/layout.jsx';
 
@@ -10,12 +17,20 @@ class Area extends Component {
     }
 
     render() {
+        const { classes, ...rest } = this.props;
+
         return (
-            <Layout>
-                <TablaPosiciones area={this.props.match.params.area} />
+            <Layout className={classes.main}>
+                <GridContainer>
+                    <GridItem>
+                        <div className={classes.container}>
+                            <TablaPosiciones area={this.props.match.params.area} />
+                        </div>
+                    </GridItem>
+                </GridContainer>
             </Layout>
         );
     }
 }
 
-export default Area;
+export default withStyles(estilos)(Area);
